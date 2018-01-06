321b17eb72d9ed841e3f1de46d06aa853d0e0e0e
Arthur Manuel Bandeira <arthurmbandeira@gmail.com>
2018-01-06 00:47:15 -0200

    setting up repository.

---
+++ index.js
@@ -0,0 +1,7 @@
+const moment = require('moment')
+
+const currencyCodes = ["AED", "ARS", "AUD", "BGN", "BND", "BOB", "BRL", "CAD", "CHF", "CLP", "CNY", "COP", "CZK", "DKK", "EGP", "EUR", "FJD", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "JPY", "KES", "KRW", "LTL", "MAD", "MXN", "MYR", "NOK", "NZD", "PEN", "PHP", "PKR", "PLN", "RON", "RSD", "RUB", "SAR", "SEK", "SGD", "THB", "TRY", "TWD", "UAH", "USD", "VEF", "VND", "ZAR"]
+
+const convertCurrency = () => {}
+
+module.exports = convertCurrency
