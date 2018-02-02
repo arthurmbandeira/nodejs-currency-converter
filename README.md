1545d40ac47387373318d4392fc10271affb7c92
Arthur Manuel Bandeira <arthurmbandeira@gmail.com>
2018-02-02 01:37:19 -0200

    fist commit to npm.

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
-# NodeJS Currency Converter [![Build Status](https://travis-ci.org/arthurmbandeira/node-currency-converter.svg?branch=master)](https://travis-ci.org/arthurmbandeira/node-currency-converter)
+# NodeJS Currency Converter
 
-A simple currency converter based on https://fixer.io.
+[![Build Status](https://travis-ci.org/arthurmbandeira/node-currency-converter.svg?branch=master)](https://travis-ci.org/arthurmbandeira/node-currency-converter)
+[![Dependencies Status](https://david-dm.org/arthurmbandeira/node-currency-converter/status.svg)](https://david-dm.org/arthurmbandeira/node-currency-converter/)
+[![DevDependencies Status](https://david-dm.org/arthurmbandeira/node-currency-converter/dev-status.svg)](https://david-dm.org/arthurmbandeira/node-currency-converter/)
+
+
+A simple currency converter based on [fixer.io](http://fixer.io).
+
+## Getting started
+
+### Installation
+This package can be installed using npm
+
+```
+npm install node-currency-converter
+```
+
+### Usage
+Import `node-currency-converter` and use it as a Promise.
+
+```
+const convertCurrency = require('node-currency-converter');
+
+convertCurrency(1, 'USD', 'BRL').then(response => response);
+```
+
+## Issues
+If any issues are found, they can be reported [here](https://github.com/arthurmbandeira/node-currency-converter/issues).
+
+## License
+
+This project is licensed under the [MIT](LICENSE) license.
--- index.js
+++ index.js
@@ -3,27 +3,28 @@ const request = require('bluebird').promisifyAll(require('request'), { multiArgs
 
 const fixerUrl = 'https://api.fixer.io';
 
-const currencyCodes = ['AED', 'ARS', 'AUD', 'BGN', 'BND', 'BOB', 'BRL', 'CAD', 'CHF', 'CLP', 'CNY', 'COP', 'CZK', 'DKK', 'EGP', 'EUR', 'FJD', 'GBP', 'HKD', 'HRK', 'HUF', 'IDR', 'ILS', 'INR', 'JPY', 'KES', 'KRW', 'LTL', 'MAD', 'MXN', 'MYR', 'NOK', 'NZD', 'PEN', 'PHP', 'PKR', 'PLN', 'RON', 'RSD', 'RUB', 'SAR', 'SEK', 'SGD', 'THB', 'TRY', 'TWD', 'UAH', 'USD', 'VEF', 'VND', 'ZAR'];
-
 const convertCurrency = (value = 1, currencyFrom, currencyTo, day) => {
   const formatedDay = (!day) ? '/latest' : moment(day).format('YYYY-MM-DD');
-  const currencyInput = [currencyFrom, currencyTo];
 
-  if (!(currencyInput.every(i => currencyCodes.includes(i)))) {
-    throw new Error('Invalid currency code.');
-  } else {
-    return new Promise(resolve => request.getAsync(`${fixerUrl}${formatedDay}?base=${currencyFrom}`).then((response) => {
-      const parsedResponse = JSON.parse(response[1]);
-      const rateFrom = parsedResponse.rates[currencyTo];
-      const convertedValue = value * rateFrom;
-      resolve({
-        currencyFrom,
-        currencyTo,
-        value,
-        convertedValue,
-      });
-    }));
-  }
+  return new Promise((resolve, reject) => request.getAsync(`${fixerUrl}/${formatedDay}?base=${currencyFrom}`).then((response) => {
+    const parsedResponse = JSON.parse(response[1]);
+
+    if (typeof value !== 'number') reject(new Error('Value to convert is NaN.'));
+    if (parsedResponse.error === 'Invalid base') {
+      reject(new Error('Invalid currency base.'));
+    } else if (!Object.keys(parsedResponse.rates).includes(currencyTo)) {
+      reject(new Error('Invalid currency to convert.'));
+    }
+
+    const rateFrom = parsedResponse.rates[currencyTo];
+    const convertedValue = value * rateFrom;
+    resolve({
+      currencyFrom,
+      currencyTo,
+      value,
+      convertedValue,
+    });
+  }));
 };
 
 module.exports = convertCurrency;
